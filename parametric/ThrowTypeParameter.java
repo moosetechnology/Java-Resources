97e414b22eb34c72d83ea689cfe6e57615ada9d8
ClotildeToullec <clotilde.toullec@inria.fr>
2024-12-19 10:43:21 +0100

    Add a case to throw a type parameter that is bounded by Throwable.

---
+++ parametric/ThrowTypeParameter.java
@@ -0,0 +1,17 @@
+package parametric;
+
+class ThrowTypeParameter<T extends Exception, U> {
+
+    /*
+     * The following method throws T, a type parameter that can be thrown because it extends Throwable (via Exception).
+     */
+    public void throwingMethod() throws T {
+    }
+
+    /*
+     * The following method is not correct because U is not bounded by Throwable.
+    public void impossibleThrowingMethod() throws U {
+    }
+    */
+
+}
